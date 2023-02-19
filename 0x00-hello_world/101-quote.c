c98aca934520538808ca0cc9b186162550409dfd
AniSmart-86 <anismart124@gmail.com>
2023-02-18 16:37:41 -0800

    line of std error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - entry point
- * return: 1
+ * main - A prgram that print a line to a standard error.
+ * return: 1 (success)
  */
 int main(void)
 {
-	write(1 "and that piece of art is useful\"
-- Dora Korpar, 2015-10-19\n", 59);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
