4977ab3b9e630e78ba3f9900fd30e2881de701c2
AniSmart-86 <anismart124@gmail.com>
2023-02-20 06:45:33 -0800

    separated numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,14 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		if (i == 9)
-			putchar(i + '0');
-		else
-		{
-			putchar(i + '0');
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(i + '0');
+		if (i != 9)
+	{
+		putchar(',');
+		putchar(' ');
 	}
+	}
+	putchar('\n');
 
 		return (0);
 }
