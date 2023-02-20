07fb2d701e56e60b6fd291cbad3b2e4836fbbe5c
AniSmart-86 <anismart124@gmail.com>
2023-02-20 01:25:47 -0800

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print an alphabet in lowercase
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+
+		return (0);
+}
