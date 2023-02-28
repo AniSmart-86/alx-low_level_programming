6ba1860ad94524a1b4f3fe178b368e3487e74330
AniSmart-86 <anismart124@gmail.com>
2023-02-28 05:09:24 -0800

    ptrint a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,11 +3,14 @@
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
+ * Return: always 0
  */
 void _puts(char *str)
 {
 	while (*str != '\0')
-	_putchar(*str++);
-	_putchar('\n');
+	{
+	putchar(*str++);
+	}
+	putchar('\n');
 }
 
