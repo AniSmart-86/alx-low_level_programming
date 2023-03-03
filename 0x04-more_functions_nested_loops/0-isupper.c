653114610da074934d73fa73d96e9db03d6f8f4e
AniSmart-86 <anismart124@gmail.com>
2023-03-03 02:30:30 -0800

    checks for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * _isupper - print the number of upper and lowercase alphabet.
- *
- * @c: parameter to print
- *
- * Return: 1 or 0
- */
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
+*/
 int _isupper(int c)
 {
-
-	if (c >= 'A' && c <= 'A')
+	if (c >= 'A' && c <= 'Z')
+	{
 	return (1);
+	}
 	else
+	{
 	return (0);
+	}
 }
-
