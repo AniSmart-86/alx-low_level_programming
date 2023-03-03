1dfbe9f8d96d145824582011cdadd0853081ab3a
AniSmart-86 <anismart124@gmail.com>
2023-03-03 01:48:12 -0800

    encodes a string

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,23 +1,23 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * rot13 - encode rot13
- *
- * @s: pointer to string parameter
+ * rot13 - encoder rot13
+ * @s: pointer to string params
  *
  * Return: *s
  */
+
 char *rot13(char *s)
 {
 	int i;
 	int j;
-	char data1[] =
-	"ABCDEFGHIJKLMNOPQRSTUWXYZabcdefghijklmnopqrstuvxwyz";
-	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLmnopqrstvwxyzabcdefghijklm";
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0 ; j < 52 ; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == data1[j])
 			{
@@ -25,8 +25,7 @@ char *rot13(char *s)
 				break;
 			}
 		}
-
 	}
-
-			return (s);
+	return (s);
 }
+
