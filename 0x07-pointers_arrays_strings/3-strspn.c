fb04e501708592bfd731a07b4c82e9ca072ca838
AniSmart-86 <anismart124@gmail.com>
2023-03-07 03:05:51 -0800

    print the length of a prefix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strspn - function definition
+ * @s: the string to search
+ * @accept: the prefic substring to be tested
+ * Description: returns number of bytes in s
+ * which only has bytes from accept
+ * Return: number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[index + 1] == '\0')
+			{
+				return (bytes);
+			}
+		}
+		s++;
+	}
+	return (bytes);
+}
