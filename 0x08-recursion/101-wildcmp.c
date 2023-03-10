fc71ea73dd316c39d97f16844e4243f90cd22d7d
AniSmart-86 <anismart124@gmail.com>
2023-03-09 17:08:42 -0800

    compare two strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * wildcmp - determine two string if identical
+ *
+ * @s1: pointer parameter
+ * @s2: pointer 2 parameter
+ *
+ * Return: 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
