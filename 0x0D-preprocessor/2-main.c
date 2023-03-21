c4ec3a6c6793d9a418454c99d6304a33a9307813
AniSmart-86 <anismart124@gmail.com>
2023-03-21 04:17:51 -0700

    a program that prints the name of the file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main -  a program that prints the name
+ *  of the file it was compiled from
+ *
+ *  Return: 0 always
+ */
+int main(void)
 {
 
 	printf("%s\n", __FILE__);
 
-	return 0;
+	return (0);
 }
