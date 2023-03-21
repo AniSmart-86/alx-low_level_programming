f0cff3af9f12089c55337c4c4861b18000b6f382
AniSmart-86 <anismart124@gmail.com>
2023-03-21 16:48:17 -0700

    dog head files

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include "dog.h"
-
 /**
 * init_dog - initializes a variable of type struct dog
+*
 * @d: pointer to struct dog to initialize
 * @name: name to initialize
 * @age: age to initialize
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-
-	/**
-	 * struct dog - a dog's basic info
-	 *
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
+/**
+* struct dog - a dog's basic info
+*
+* @name: First member
+* @age: Second member
+* @owner: Third member
+*
+* Description: Longer description
+*/
 	struct dog
 	{
 		char *name;
@@ -19,10 +18,10 @@
 	};
 
 
-	/**
-	 * dog_t - typedef for struct dog
-	 */
-	typedef struct dog dog_t;
+/**
+* dog_t - typedef for struct dog
+*/
+typedef struct dog dog_t;
 
 
 	void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -33,5 +32,5 @@
 	int _strlen(char *s);
 
 
-	#endif
+#endif
 
