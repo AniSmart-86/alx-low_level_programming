44099f17f442a53d977d4493076ca717c5ef504f
AniSmart-86 <anismart124@gmail.com>
2023-04-05 18:07:56 -0700

    add nodeint

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Function that calculates the sum
+ * of all the data in a listint_t list
+ * @head: first nodes
+ *
+ * Return:sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+	sum += temp->n;
+	temp = temp->next;
+	}
+
+	return (sum);
+}
